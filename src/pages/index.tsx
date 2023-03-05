e37fe87c46b5956dab6f755bad54260431644e5b
Christopher Duncan <chris.duncan.arauz+git@protonmail.com>
2023-03-05 17:12:49 -0500

    Add ESLint, Cypress, Jest, and basic form in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,40 @@
-export default function Index() {
-	return <div/>
+import React from "react"
+
+export default function Index(): JSX.Element {
+
+	const [hasSubmitted, setHasSubmitted] = React.useState(false)
+
+	const onSubmit: React.FormEventHandler = (event) => {
+		event.preventDefault()
+
+		setHasSubmitted(true)
+	}
+
+	return (
+		<React.Fragment>
+			<form onSubmit={onSubmit}>
+				<div>
+					<label htmlFor={imagesId}>Select Image</label>
+					<input
+						id={imagesId}
+						type="file"
+						accept="image/*"
+						capture
+						multiple
+					/>
+				</div>
+				<div>
+					<label htmlFor={descriptionId}>Image Description</label>
+					<input id={descriptionId} />
+				</div>
+				<button type="submit">Generate Report</button>
+			</form>
+			{hasSubmitted && (
+				<span>Report was generated.</span>
+			)}
+		</React.Fragment>
+	)
 }
+
+const imagesId = "images"
+const descriptionId = "description"
