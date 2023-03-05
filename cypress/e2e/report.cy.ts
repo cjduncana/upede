dd1fd42f2efd2bdb7f84833b6ba133dc335eb03e
Christopher Duncan <chris.duncan.arauz+git@protonmail.com>
2023-03-05 17:12:49 -0500

    Add ESLint, Cypress, Jest, and basic form in page

---
+++ cypress/e2e/report.cy.ts
@@ -0,0 +1,17 @@
+describe("Report", () => {
+
+	beforeEach(() => {
+		cy.visit("/")
+	})
+
+	it("should create a Report", () => {
+		cy.fixture("pothole.jpeg").as("pothole")
+		cy.findByLabelText("Select Image").selectFile("@pothole")
+
+		cy.findByRole("textbox", { name: "Image Description" }).type("There's a pothole!")
+
+		cy.findByRole("button", { name: "Generate Report" }).click()
+
+		cy.findByText("Report was generated.").should("exist")
+	})
+})
