c8693aba76df68157c84b61710bb90c9f2df0fc9
Christopher Duncan <chris.duncan.arauz+git@protonmail.com>
2023-03-05 22:19:20 -0500

    Use Material UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
+import { Alert, Button, Stack, TextField } from "@mui/material"
+import { useTheme } from "@mui/material/styles"
 import React from "react"
 
 export default function Index(): JSX.Element {
+
+	const theme = useTheme()
 
 	const [hasSubmitted, setHasSubmitted] = React.useState(false)
 
@@ -12,29 +16,28 @@ export default function Index(): JSX.Element {
 
 	return (
 		<React.Fragment>
-			<form onSubmit={onSubmit}>
-				<div>
-					<label htmlFor={imagesId}>Select Image</label>
+			{hasSubmitted && (
+				<Alert severity="success">Report was generated.</Alert>
+			)}
+			<Stack
+				component="form"
+				onSubmit={onSubmit}
+				spacing={2}
+				sx={{ margin: "0 auto", padding: theme.spacing(2), maxWidth: 1024 }}
+			>
+				<Button component="label">
+					Select Image
 					<input
-						id={imagesId}
+						hidden
 						type="file"
 						accept="image/*"
 						capture
 						multiple
 					/>
-				</div>
-				<div>
-					<label htmlFor={descriptionId}>Image Description</label>
-					<input id={descriptionId} />
-				</div>
-				<button type="submit">Generate Report</button>
-			</form>
-			{hasSubmitted && (
-				<span>Report was generated.</span>
-			)}
+				</Button>
+				<TextField label="Image Description" multiline rows={4} />
+				<Button type="submit" variant="contained">Generate Report</Button>
+			</Stack>
 		</React.Fragment>
 	)
 }
-
-const imagesId = "images"
-const descriptionId = "description"
